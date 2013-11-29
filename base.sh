c6f2618afb3da7fb9cb3d9c0aebb05f26a9cd645
paul <paul@ubuntu.(none)>
2013-11-29 15:26:04 +0100

    Correct ownership of amcat's home-directory

--- base.sh
+++ base.sh
@@ -5,11 +5,15 @@ echo "Installing basic features."
 apt-get install -y curl software-properties-common python-software-properties git python-pip
 
 echo "Checking whether user $AMCAT_USER exists"
+# The directory that contains $AMCAT_ROOT will be the user's home directory
 getent passwd $AMCAT_USER  > /dev/null
 if [ $? -eq 2 ]; then
     echo "Creating user..."
     set -e
-    useradd -Ms/bin/bash $AMCAT_USER
+    USER_HOME=`dirname $AMCAT_ROOT` 
+    useradd -Ms/bin/bash --home "$USER_HOME" $AMCAT_USER
+    mkdir -p $USER_HOME
+    chown amcat:amcat $USER_HOME
 fi
 set -e
 
